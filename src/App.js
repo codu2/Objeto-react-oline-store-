2bc57373949ebc434721ac44f0638af096d40c7e
codu2 <82283517+codu2@users.noreply.github.com>
2021-11-23 22:42:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+  
   return (
-    <div className="App">
-      <Header />
-      <Products />
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Products />
+      </main>
+    </CartProvider>
   );
 }
 
